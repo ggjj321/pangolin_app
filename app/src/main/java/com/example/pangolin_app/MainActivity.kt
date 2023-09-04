a7977686ffbea951ec2bd1c3da7ad191e73cb1c2
ggjj321 <10370324lsjh@gmail.com>
2023-09-04 14:03:54 +0800

    add activate

--- app/src/main/java/com/example/pangolin_app/MainActivity.kt
+++ app/src/main/java/com/example/pangolin_app/MainActivity.kt
@@ -1,21 +1,38 @@
 package com.example.pangolin_app
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.KeyEvent.KEYCODE_DPAD_DOWN
 import android.view.KeyEvent.KEYCODE_DPAD_UP
 import android.view.MotionEvent
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.pangolin_app.ui.theme.Pangolin_appTheme
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -24,6 +41,9 @@ import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.net.Socket
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
 class ClientSo(host: String, port: Int) : Socket(host, port) {
     var reader = BufferedReader(InputStreamReader(inputStream))
@@ -51,8 +71,14 @@ class MainActivity : ComponentActivity() {
     val port = 8000
     private lateinit var client: ClientSo
     var axisMap = mutableMapOf<String, Float?>()
+    val activate = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val sensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+
         setContent {
             Pangolin_appTheme {
                 // A surface container using the 'background' color from the theme
@@ -60,7 +86,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("穿山甲")
+                    var textVisible by remember { mutableStateOf(true) }
+
+                    val sensorListener = object: SensorEventListener {
+                        override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
+
+                        override fun onSensorChanged(event: SensorEvent?) {
+                            if(event != null){
+                                val zValue = Math.abs(event.values[2]) // 加速度 - Z 軸方向
+
+                                if (zValue > 2) textVisible = !textVisible
+
+                                println(textVisible)
+                            }
+                        }
+                    }
+                    sensorManager.registerListener(sensorListener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+
+                    Greeting("穿山甲", textVisible)
                 }
             }
         }
@@ -69,10 +112,10 @@ class MainActivity : ComponentActivity() {
         axisMap["zAxis"] = 0.0f
         axisMap["rzAxis"] = 0.0f
 
-        Thread(Runnable {
-            client = ClientSo(host, port)
-        }).start()
-        Thread(Runnable { socketSend() }).start()
+//        Thread(Runnable {
+//            client = ClientSo(host, port)
+//        }).start()
+//        Thread(Runnable { socketSend() }).start()
     }
 
     fun socketSend() {
@@ -108,20 +151,29 @@ class MainActivity : ComponentActivity() {
         }
         return super.dispatchKeyEvent(event)
     }
+
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Pangolin_appTheme {
-        Greeting("Android")
+fun Greeting(name: String, textVisible: Boolean, modifier: Modifier = Modifier) {
+    AnimatedVisibility(
+        visible = textVisible,
+        enter = fadeIn(animationSpec = tween(1000)),
+        exit = fadeOut(animationSpec = tween(1000))
+    ) {
+        Text(
+            text = name,
+            modifier = Modifier.padding(160.dp),
+            style = TextStyle(fontSize = 160.sp, color = Color.Black),
+            textAlign = TextAlign.Center
+        )
     }
 }
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    Pangolin_appTheme {
+//        Greeting("Android", textVisible = )
+//    }
+//}
